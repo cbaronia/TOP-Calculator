076e648f6604d3b9a692e12c6b809750613bbef9
cbaronia <86375882+cbaronia@users.noreply.github.com>
2024-07-21 13:18:58 -0700

    Basic calculation w/ equal button and A/C functional

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // GLOBAL VARIABLES
-let firstNum = 9;
-let operator = '/';
-let secondNum = 2;
+let firstNum = 0;
+let operator = '';
 
-let displayValue = "";
+let displayValue = "0";
+let firstNumSet = false;
 
 function add(a, b){
     return a + b;
@@ -45,8 +45,14 @@ function operate(a, b, op){
     return opResult;
 }
 
+// Query Selectors
+let myDisplay = document.querySelector(".display");
+let numberBtns = document.querySelectorAll(".numbers > button");
+let calcBtns = document.querySelectorAll(".calcButtons > button");
+let equalBtn = document.querySelector(".equalBtn");
+let clearBtn = document.querySelector(".clearBtn");
+
 function updateDisplayValue_Number(btnValue){
-    let myDisplay = document.querySelector(".display");
 
     // First button was zero
     if(displayValue === "0" && btnValue === "0"){
@@ -64,15 +70,39 @@ function updateDisplayValue_Number(btnValue){
     myDisplay.textContent = displayValue;
 }
 
-// Query Selectors
-let numberBtns = document.querySelectorAll(".numbers > button");
-
 // Event Listeners
 numberBtns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
+    btn.addEventListener("click", () => {
         
         // Update the displayValue on screen
         updateDisplayValue_Number(btn.value);
 
     });
-})
+});
+
+calcBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if(!firstNumSet){
+            firstNum = +displayValue;
+            firstNumSet = true;
+            displayValue = "";
+        }
+        operator = btn.value;
+    });
+});
+
+equalBtn.addEventListener("click", () => {
+    let calcResult = operate(firstNum, +displayValue, operator);
+    displayValue = calcResult.toString();
+    myDisplay.textContent = displayValue;
+    firstNum = calcResult;
+    displayValue = "";
+});
+
+clearBtn.addEventListener("click", () => {
+    firstNum = 0;
+    operator = '';
+    displayValue = "0";
+    firstNumSet = false;
+    myDisplay.textContent = displayValue;
+});
